bca8b568167edf71279576cf284538b5356c032a
IseanB <Isean4B@gmail.com>
2025-12-04 22:47:10 +0000

    personalized training

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,12 +1,21 @@
+import sys
+import os
+from pathlib import Path
+
+# Add the src directory to the Python path
+sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
+
+# Set PyTorch memory allocation to avoid fragmentation
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
 modelName = 'speechBaseline4'
 
 args = {}
-args['outputDir'] = '/oak/stanford/groups/henderj/stfan/logs/speech_logs/' + modelName
-args['datasetPath'] = '/oak/stanford/groups/henderj/fwillett/speech/ptDecoder_ctc'
+args['outputDir'] = '/home/iseanbhanot/output_dir' + modelName
+args['datasetPath'] = '/home/iseanbhanot/dataset/ptDecoder_ctc.pkl'
 args['seqLen'] = 150
 args['maxTimeSeriesLen'] = 1200
-args['batchSize'] = 64
+args['batchSize'] = 32  # Reduced from 64 to fit in GPU memory
 args['lrStart'] = 0.02
 args['lrEnd'] = 0.02
 args['nUnits'] = 1024
